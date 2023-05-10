3ba41ce376c85f5f9f8d61d696842785f743e76d
Андрей Тарасов <baby-shit@yandex.ru>
2023-05-10 12:24:42 +0300

    redoc

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -121,7 +121,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'asserts')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'asserts')
 
 AUTH_USER_MODEL = 'users.User'
 
